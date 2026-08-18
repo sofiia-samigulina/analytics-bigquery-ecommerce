a64d6e92012da92d2264eecfea37005d0a96d0e7
Sofiia Samigulina <sofiia.samigulina@student.tuke.sk>
2026-08-18 15:01:50 +0200

    Added partitioned tables and 2025 year for holidays. Needs to debug, because there are more years in orders

--- data-pipeline/data-ingestion/loader.py
+++ data-pipeline/data-ingestion/loader.py
@@ -1,12 +1,12 @@
 import requests
 from google.oauth2 import service_account as sa
 from google.cloud import bigquery as bq
+import time
 
 #project details
-PROJECT_ID = 'project-1fe4418a-24e0-4804-a77'
+MY_PROJECT_ID = 'project-1fe4418a-24e0-4804-a77'
+ECOMMERCE_DATASET_ID = 'bigquery-public-data.thelook_ecommerce'
 DATASET_ID = 'raw_ecommerce'
-TABLE_ID = 'holidays'
-TABLE_NAME = f'{DATASET_ID}.{TABLE_ID}'
 URL_HOLIDAYS_BASE = 'https://nagerholidays.com/api/v4/Holidays/' #API without a key for access
 SERVICE_ACCOUNT_FILE = 'data-pipeline/data-ingestion/service_account_credential.json' #path to service account file
 
@@ -14,28 +14,33 @@ SERVICE_ACCOUNT_FILE = 'data-pipeline/data-ingestion/service_account_credential.
 def get_the_holidays():
     #only countries from the dataset
     countries_codes = ["BR", "JP", "US", "CO", "ES", "CN", "AU", "FR", "DE", "BE", "KR", "PL", "GB", "AT"] 
+    countries_total = len(countries_codes)
+
+    years = ["2025", "2026"] #TODO: try calendrafic besause it has more years (from 2019)
+    years_total = len(years)
     
     holidays_list = []
-    countries_count = 0
-    for country in countries_codes:
-        current_url = URL_HOLIDAYS_BASE + country + '/2026'
-        r = requests.get(current_url, auth=('user', 'pass'))
-        #TODO: add the log file
-        if r.status_code != 200:
-            print(f'For {country} a response failed')
-            continue
-        holidays_list.extend(r.json())
-        countries_count += 1
 
-    return holidays_list, countries_count
+    for year in years:
+        country_count = 0
+        print(f'Getting the holidays for the year {year}...')
+        for country in countries_codes:
+            current_url = URL_HOLIDAYS_BASE + country + f'/{year}'
+            r = requests.get(current_url, auth=('user', 'pass'))
+            #TODO: add the log file
+            if r.status_code != 200:
+                print(f'For {country} and year {year} a response failed')
+                continue
+            holidays_list.extend(r.json())
+            if r.status_code == 200:
+                country_count += 1
+        print(f'For the year {year}: {country_count} countries were successful.')
+        time.sleep(1) #to avoid the API limit
+
+    return holidays_list
 
 def load_holidays_to_bigquery():
-    print("Getting the list of holidays...")
-    holidays_list, countries_count = get_the_holidays()
-    if countries_count == 14:
-        print("All of the countries were successful")
-    else:
-        print(f"Only {countries_count} countries were successful")
+    holidays_list = get_the_holidays()
 
     job_config = bq.LoadJobConfig(
         write_disposition=bq.WriteDisposition.WRITE_TRUNCATE,
@@ -46,18 +51,56 @@ def load_holidays_to_bigquery():
             bq.SchemaField("subdivisionCodes", "STRING", mode="REPEATED"),
             bq.SchemaField("nationalHoliday", "BOOLEAN"),
             bq.SchemaField("holidayTypes", "STRING", mode="REPEATED")
-        ],
-        create_disposition=bq.CreateDisposition.CREATE_IF_NEEDED
+        ]
     )
     print("Loading holidays to BigQuery...")
-    job = client.load_table_from_json(holidays_list, TABLE_NAME, job_config = job_config)
+    job = client.load_table_from_json(holidays_list, f'{DATASET_ID}.holidays', job_config = job_config)
     print("Loading holidays was successful")
-    
+
+def create_query_partitions(table_id, partition_key):
+    query = f"""
+        CREATE OR REPLACE TABLE `{DATASET_ID}.{table_id}`
+        PARTITION BY DATE({partition_key})
+        AS
+        SELECT * FROM `{ECOMMERCE_DATASET_ID}.{table_id}`;
+    """
+    return query
+
+def create_base_query(table_id):
+    query = f"""
+        CREATE OR REPLACE TABLE `{DATASET_ID}.{table_id}`
+        AS
+        SELECT * FROM `{ECOMMERCE_DATASET_ID}.{table_id}`;
+    """
+    return query
+
 if __name__=="__main__":
     
+    #1 Connecting to BigQuery
     print("Connecting to the cloud")
     credentials = sa.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
-    client = bq.Client(credentials=credentials, project=PROJECT_ID)
+    client = bq.Client(credentials=credentials, project=MY_PROJECT_ID)
     print("Connecting was successful")
 
+    #2 Loading the holidays to BigQuery
     load_holidays_to_bigquery()
+
+    #3 Ingesting the tables from thelook_ecommerce
+
+    #creating partitions for tables and loading
+    tables_to_partition = ['orders', 'order_items', 'events', 'inventory_items']
+    for table in tables_to_partition:
+        print(f"Partitioning and loading the table {table}...")
+        query = create_query_partitions(table, 'created_at')
+        query_job = client.query(query)
+        rows = query_job.result()  # Waits for query to finish
+        print(f"Partitioning and loading the table {table} was successful")
+
+    #loading the rest of the tables without partitions
+    tables_to_load = ['products', 'users', 'distribution_centers']
+    for table in tables_to_load:
+        print(f"Loading the table {table}...")
+        query = create_base_query(table)
+        query_job = client.query(query)
+        rows = query_job.result()  # Waits for query to finish
+        print(f"Loading the table {table} was successful")
